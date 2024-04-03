0ba2be62e841480ba7eb4ad7c32c1019a6aa4591
Matheus Bastos Nhoncanse <matheus.nhoncanse@aluno.ep.com>
2024-04-03 10:21:43 -0300

    Mais ex foi adicionados

---
+++ aula010/ex008.js
@@ -0,0 +1,8 @@
+var vel = 12 
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if (vel >= 60) {
+    console.log('Você ultrapassou a velocidade permetida, MULTADO!')
+    console.log('Sua multa e de 100,00R$')
+}
+console.log('Diriga sempre com cinto de segurança')
+//condição simples
