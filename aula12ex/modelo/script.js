1de3e4e2da83f76f4cdf9860398300d690d24bb2
Matheus Bastos Nhoncanse <matheus.nhoncanse@aluno.ep.com>
2024-04-17 10:17:06 -0300

    Acabou o aula12ex

--- aula12ex/modelo/script.js
+++ aula12ex/modelo/script.js
@@ -1,1 +1,22 @@
-alert('ola')
+function carregar() {
+    var msg = document.getElementById('msg')
+    var img = document.getElementById('img')
+    var data = new Date()
+    var hora =  23//data.getHours()
+    msg.innerHTML = `Agora são <strong>${hora} horas</strong>.`
+
+    if (hora >= 0 && hora < 12 ) {
+        img.src = 'imagem/fotoa.jpg'
+        document.body.style.background = '#E5DBD6'
+    }
+    else if (hora >= 12 && hora < 18) {
+        img.src = 'imagem/fotot.jpg'
+        document.body.style.background = '#D7AF70'
+    } else {
+        img.src = 'imagem/foton.jpg'
+        document.body.style.background = '#061616'
+        document.querySelector('body header h1').style.color = 'white';
+        document.querySelector('body footer p').style.color = 'white';
+
+    }
+}
